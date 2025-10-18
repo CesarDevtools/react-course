b142130955836c81ba4ceaf03118c1c05e05dbc8
Cesar <cessargalan@gmail.com>
2025-10-18 19:17:12 +0100

    React Router, Git, Start the ecommerce Project

---
+++ ecommerce-project/src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from 'react-router'
+import { HomePage } from './pages/HomePage.jsx'
+import { CheckoutPage } from './pages/CheckoutPage.jsx' 
+import { OrdersPage } from './pages/OrdersPage.jsx'
+import { TrackingPage } from './pages/TrackingPage.jsx'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <Routes>
+
+      <Route index element={<HomePage />}></Route>
+      <Route path="checkout" element={<CheckoutPage />}></Route>
+      <Route path="orders" element={<OrdersPage />}></Route>
+      <Route path="tracking" element={<TrackingPage />}></Route>
+
+    </Routes>
+    
+  )
+}
+
+export default App
